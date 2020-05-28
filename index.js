f7d1f83c777f9289c97c139b291fab4f03f2a8cd
Henry <hp@henryprescott.dev>
2020-05-28 17:39:06 +0100

    thinking about next steps

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ client.on("message", async msg => {
         msg.delete({ timeout: 100 });
 
         try {
+            // TODO
+            // 1. build up role list from raid-helper bot reactions (as roles & classes may vary across factions etc)
+            // 2. build map/array of roles & map/array of sign-up order
+            // 3. iterate through embed fields to extract role counts, order and populate above
 
+            // replace with event tracking/searching - currently hardcoded to a specific message
             const eventMessage = await client.channels.cache.get("714872746072473621").messages.fetch("715509947214856252");
 
-            const embedFields = eventMessage.embeds[0].fields;
-
-            for (let field in embedFields) {
-                console.log(embedFields[field].value)
-            }
-
+            /* Repurpose following code for point 1. */
             const eventReactions = await eventMessage.reactions.cache.array();
 
             for (let reaction in eventReactions) {
@@ -37,6 +37,19 @@ client.on("message", async msg => {
                     }
                 }
             }
+            /* Repurpose above code for point 1. */
+
+            // create stuff for 2. here
+
+            /* Repurpose following code for point 3. */
+            const embedFields = eventMessage.embeds[0].fields;
+
+            for (let field in embedFields) {
+                // need to do some REGEX here to grab useful info from value?
+                console.log(embedFields[field].value)
+            }
+            /* Repurpose above code for point 3. */
+
         } catch (error) {
             console.log(`failed to count roles: ${error}`);
         }
