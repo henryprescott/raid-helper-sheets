e97288856645d736fe985ee8f296d009fc3beece
Henry <hp@henryprescott.dev>
2020-06-02 10:19:00 +0100

    Code fix

--- index.js
+++ index.js
@@ -121,7 +121,6 @@ async function updateEventSheet(event_sheet, sign_up_order, raid_helper_reaction
     for (let i = 0; i < raid_helper_reactions.length; i++) {
         const role_title = event_sheet.getCell(i + 1, 2);
         role_title.value = raid_helper_reactions[i];
-        let role_sign_up_data = role_sign_up_data;
         for (let j = 0; j < role_sign_up_data[raid_helper_reactions[i]].length; j++) {
             // console.log(`Cell: ${i}, ${j} - ${role_sign_up_data[raid_helper_reactions[i]][j]}`);
             if (event_sheet != null) {
@@ -307,25 +306,26 @@ async function getEventReactions(event_message) {
 
 /* Discord JS specific code */
 
-client.on("messageReactionAdd", async (reaction, user) => {
-    // if(reaction.message.author.bot && reaction.message.author.username === "Raid-Helper") {
-    // When we receive a reaction we check if the reaction is partial or not
-    if (reaction.partial) {
-        // If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
-        try {
-            await reaction.fetch();
-        } catch (error) {
-            console.log('Something went wrong when fetching the message: ', error);
-            // Return as `reaction.message.author` may be undefined/null
-            return;
-        }
-    }
-    // Now the message has been cached and is fully available
-    console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`);
-    // The reaction is now also fully available and the properties will be reflected accurately:
-    console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
-    // }
-})
+// commented for now in case it triggers too much server usage
+// client.on("messageReactionAdd", async (reaction, user) => {
+//     // if(reaction.message.author.bot && reaction.message.author.username === "Raid-Helper") {
+//     // When we receive a reaction we check if the reaction is partial or not
+//     if (reaction.partial) {
+//         // If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
+//         try {
+//             await reaction.fetch();
+//         } catch (error) {
+//             console.log('Something went wrong when fetching the message: ', error);
+//             // Return as `reaction.message.author` may be undefined/null
+//             return;
+//         }
+//     }
+//     // Now the message has been cached and is fully available
+//     console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`);
+//     // The reaction is now also fully available and the properties will be reflected accurately:
+//     console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
+//     // }
+// })
 
 client.on("message", async msg => {
     if (msg.content === "!roleCount") {
